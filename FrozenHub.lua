eb9e0fe363c3d48e18e1ee99600d1ef7b574810f
K1NM4 <94777469+K1NM4@users.noreply.github.com>
2021-11-22 11:34:45 +0700

    Update FrozenHub.lua

--- FrozenHub.lua
+++ FrozenHub.lua
@@ -1,77 +1,84 @@
-local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/discord%20lib.txt")()
+-- init
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/K1NM4/gui/main/Gui.txt"))()
+local venyx = library.new("Frozen Hub", 5013109572)
 
-local win = DiscordLib:Window("Frozen Hub")
+-- themes
+local themes = {
+Background = Color3.fromRGB(0, 34, 36),
+Glow = Color3.fromRGB(0, 95, 104),
+Accent = Color3.fromRGB(10, 10, 10),
+LightContrast = Color3.fromRGB(0, 34, 36),
+DarkContrast = Color3.fromRGB(14, 14, 14),  
+TextColor = Color3.fromRGB(0, 255, 244)
+}
 
-local serv = win:Server("Main", "http://www.roblox.com/asset/?id=6031075938")
+-- first page
+local page = venyx:addPage("MAIN", 5012544693)
+local AllStar = page:addSection("All Star")
+local Arsenal = page:addSection("Arsenal")
+local CounterBlox = page:addSection("Counter Blox")
+local Dahood = page:addSection("Da hood")
 
-local astd = serv:Channel("All Star")
-
-astd:Button("Karma Panda", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+AllStar:addButton("Karma Panda", function()
 print(loadstring(game:HttpGet('https://raw.githubusercontent.com/KarmaPanda/Roblox/main/astd-aio.lua'))())
 end)
-
-astd:Seperator()
-
-astd:Button("IDK", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+AllStar:addButton("IDK", function()
 print(loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperOuro/Lua-Scripts/main/all_star_tower_defense.lua"))())
 end)
 
-local an = serv:Channel("Arsenal")
-
-an:Button("V.G. Hub", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+Arsenal:addButton("V.G. Hub", function()
 print(loadstring(game:HttpGet('https://raw.githubusercontent.com/1201for/V.G-Hub/main/V.Ghub'))())
 end)
 
-an:Seperator()
-
-an:Button("Dark Hub", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+Arsenal:addButton("Dark Hub", function()
 print(loadstring(game:HttpGet("https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init", true))())
 end)
 
-local cb = serv:Channel("Counter Blox")
-
-cb:Button("Dark Hub", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+CounterBlox:addButton("Dark Hub", function()
 print(loadstring(game:HttpGet("https://raw.githubusercontent.com/RandomAdamYT/DarkHub/master/Init", true))())
 end)
 
-cb:Seperator()
-
-cb:Button("IDK", function()
-DiscordLib:Notification("Notification", "Success!", "Okay!")
+CounterBlox:addButton("IDK", function()
 print(loadstring(game:HttpGet('https://raw.githubusercontent.com/venosu/Hex-Hub/main/hexhub.lua'))())
 end)
-    
-local dh = serv:Channel("Da Hood")
 
-dh:Button("IDK", function()
-DiscordLib:Notification("Notification", "Success!","Okay!")
+Dahood:addButton("IDK", function()
 print(loadstring(game:HttpGet("https://raw.githubusercontent.com/nighter132/Faded/main/YesEpic", true))())
 end)
 
-dh:Seperator()
-
-dh:Button("IDK", function()
-DiscordLib:Notification("Notification", "Success!","Okay!")
+Dahood:addButton("IDK", function()
 print(loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\34\104\116\116\112\115\58\47\47\114\97\119\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\86\97\110\105\115\50\53\50\47\118\97\108\105\97\110\116\47\109\97\105\110\47\46\103\105\116\105\103\110\111\114\101\34\41\44\32\116\114\117\101\41\41\40\41\10")())
 end)
 
-local bnds = serv:Channel("KeyBinds")
+-- second page
+local page = venyx:addPage("MISC", 6306230437)
+local ui = page:addSection("Toggle UI")
 
-bnds:Bind("Key bind", Enum.KeyCode.RightControl, function()
-Library:ToggleUI()
+ui:addKeybind("Toggle Keybind", Enum.KeyCode.RightControl, function()
+print("Activated Keybind")
+venyx:toggle()
+end, function()
+print("Changed Keybind")
 end)
 
-local cred = serv:Channel("Credit")
+-- third page
+local theme = venyx:addPage("Theme", 5012544693)
+local colors = theme:addSection("Colors")
 
-cred:Button("Copy Discord Server",function()
-setclipboard("https://discord.gg/e54pdXBH2B")
-end) 
-    
-cred:Label("SCRIPT BY ! K1NM4#0001")
+for theme, color in pairs(themes) do -- all in one theme changer, i know, im cool
+colors:addColorPicker(theme, color, function(color3)
+venyx:setTheme(theme, color3)
+end)
+end
 
-cred:Label("GUI BY dawid#7205")
+-- fourth page
+local page = venyx:addPage("Credit", 6031075938)
+local cred = page:addSection("Credit")
+
+cred:addButton("DISCORD - ! K1NM4#0001", function()
+print("Discord")
+end)
+
+cred:addButton("DISCORD - e54pdXBH2B", function()
+print("Discord Server")
+end)
